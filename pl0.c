36e4b89bb03c6cd18c0e65a5a0abdf50afc6ab53
DJM199708 <47919778+DJM199708@users.noreply.github.com>
2019-07-03 09:26:21 +0800

    新增pl0.h头文件注释

--- pl0.c
+++ pl0.c
@@ -4,19 +4,655 @@
 #include <string.h>
 
 #include "pl0.h"
-#include "error/error.c"
-#include "getsym/getsym.c"
-#include "gen/gen.c"
-#include "test/test.c"
-#include "block/block.c"
-#include "interpret/interpret.c"
 
-int main() {
+/* º¯ÊýÉùÃ÷ */
+void error(long n);
+void getch();
+void getsym();
+void gen(enum fct x, long y, long z);
+void test(unsigned long s1, unsigned long s2, long n);
+void enter(enum object k); // enter object into table
+long position(char * id); // find identifier id in table
+void constdeclaration();
+void vardeclaration();
+void listcode(long cx0); // list code generated for this block
+void factor(unsigned long fsys); //ÒÀÀµexpression()
+void term(unsigned long fsys);
+void expression(unsigned long fsys);
+void condition(unsigned long fsys);
+void statement(unsigned long fsys);
+void block(unsigned long fsys);
+long base(long b, long l);
+void interpret();
+
+void error(long n) {
     long i;
-    for (i = 0; i < 256; i++) { //æŸä¸ªæ•°ç»„çš„åˆå§‹åŒ–ï¼Ÿ
+
+    printf(" ****");
+    for (i = 1; i <= cc - 1; i++) {
+        printf(" ");
+    }
+    printf("^%2d\n", n);
+    err++;
+}
+
+void getch() { //»ñÈ¡µ¥¸ö×Ö·û£¬ÐÐ»º³å¶ÁÍê×Ô¶¯¼ÓÔØÏÂÒ»ÐÐ
+    if (cc == ll) { // cc == ll Ê±£¬×Ö·û¶ÁÈ¡ÒÑ¶Áµ½µ±Ç°ÐÐ»º³åÇøµÄÄ©¶Ë£¬Ðè¼ÓÔØ£¨ÏÂÒ»ÐÐ£©
+        if (feof(infile)) { //¼ì²âÁ÷ÉÏµÄÎÄ¼þ½áÊø·û£¬Èç¹ûÎÄ¼þ½áÊø£¬Ôò·µ»Ø·Ç0Öµ£¬·ñÔò·µ»Ø0
+            printf("************************************\n");
+            printf("      program incomplete\n");
+            printf("************************************\n");
+            exit(1);
+        }
+        ll = 0;
+        cc = 0;
+        printf("%5d ", cx);
+        while ((!feof(infile)) && ((ch = getc(infile)) != '\n')) {
+            printf("%c", ch);
+            ll = ll + 1; //ÐÐÄÚ·ûºÅ¼ÆÊý
+            line[ll] = ch; //ÐÐ»º³åÇø
+        }
+        printf("\n");
+        ll = ll + 1;
+        line[ll] = ' ';
+    }
+    cc = cc + 1;
+    ch = line[cc];
+}
+
+void getsym() {
+    long i, j, k;
+
+    while (ch == ' ' || ch == '\t') { //¹ýÂË¿Õ¸ñºÍÖÆ±í·û ch´æ·Åµ±Ç°È¡³öÀ´µÄ·ûºÅ
+        getch();
+    }
+
+    // ÒÀ´ÎÅÐ¶ÏÊÇ·ñÎª ×ÖÄ¸ Êý×Ö : < > {+ - * / ( ) = , . ;}
+    if (isalpha(ch)) { // identified or reserved Ð¡Ð´×ÖÄ¸Îª2£¬´óÐ´×ÖÄ¸Îª1£¬Èô²»ÊÇ×ÖÄ¸£¬·µ»Ø0
+        k = 0;
+        do {
+            if (k < al) {
+                a[k] = ch;
+                k = k + 1;
+            }
+            getch();
+        } while (isalpha(ch) || isdigit(ch));
+        if (k >= kk) { //ÒÉËÆÎªa[]µÄ¿Õ´¦(NULL)Ìî³ä¿Õ¸ñ·û
+            kk = k;
+        } else {
+            do {
+                kk = kk - 1;
+                a[kk] = ' ';
+            } while (k < kk);
+        }
+        strcpy(id, a);// id´Óa¿½±´µ¥´Ê£¨Ò»¸öµ¥´Ê£©
+        i = 0;
+        j = norw - 1;
+        do { //¶þ·Ö²éÕÒ±£Áô×Ö
+            k = (i + j) / 2;
+            if (strcmp(id, word[k]) <= 0) {
+                j = k - 1;
+            }
+            if (strcmp(id, word[k]) >= 0) {
+                i = k + 1;
+            }
+        } while (i <= j);
+        if (i - 1 > j) { //±£Áô×ÖÆ¥Åä³É¹¦
+            sym = wsym[k];
+        } else { //±£Áô×ÖÆ¥ÅäÊ§°Ü£¬Ôòµ±Ç°µ¥´ÊÊÇ±êÊ¶·û£¨³£Á¿»ò±äÁ¿£©
+            sym = ident;
+        }
+    } else if (isdigit(ch)) { // number ¼ì²échÊÇ·ñÎªÊ®½øÖÆÊý×Ö×Ö·û¡£
+        k = 0;
+        num = 0;
+        sym = number;
+        do {
+            num = num * 10 + (ch - '0');
+            k = k + 1;
+            getch();
+        } while (isdigit(ch));
+        if (k > nmax) {
+            error(31);
+        }
+    } else if (ch == ':') {
+        getch();
+        if (ch == '=') {
+            sym = becomes;
+            getch();
+        } else {
+            sym = nul;
+        }
+    } else if (ch == '<') {
+        getch();
+        if (ch == '=') {
+            sym = leq;
+            getch();
+        } else if (ch == '>') {
+            sym = neq;
+            getch();
+        } else {
+            sym = lss;
+        }
+    } else if (ch == '>') {
+        getch();
+        if (ch == '=') {
+            sym = geq;
+            getch();
+        } else {
+            sym = gtr;
+        }
+    } else {
+        sym = ssym[(unsigned char) ch];
+        getch();
+    }
+}
+
+void gen(enum fct x, long y, long z) {
+    if (cx > cxmax) {
+        printf("program too long\n");
+        exit(1);
+    }
+    code[cx].f = x;
+    code[cx].l = y;
+    code[cx].a = z;
+    cx = cx + 1;
+}
+
+void test(unsigned long s1, unsigned long s2, long n) {
+    if (!(sym & s1)) {
+        error(n);
+        s1 = s1 | s2;
+        while (!(sym & s1)) {
+            getsym();
+        }
+    }
+}
+
+void enter(enum object k) { // enter object into table
+    tx = tx + 1;
+    strcpy(table[tx].name, id);
+    table[tx].kind = k;
+    switch (k) {
+        case constant:
+            if (num > amax) {
+                error(31);
+                num = 0;
+            }
+            table[tx].val = num;
+            break;
+        case variable:
+            table[tx].level = lev;
+            table[tx].addr = dx;
+            dx = dx + 1;
+            break;
+        case proc:
+            table[tx].level = lev;
+            break;
+    }
+}
+
+long position(char * id) { // find identifier id in table
+    long i;
+
+    strcpy(table[0].name, id);
+    i = tx;
+    while (strcmp(table[i].name, id) != 0) {
+        i = i - 1;
+    }
+    return i;
+}
+
+void constdeclaration() {
+    if (sym == ident) {
+        getsym();
+        if (sym == eql || sym == becomes) {
+            if (sym == becomes) {
+                error(1);
+            }
+            getsym();
+            if (sym == number) {
+                enter(constant);
+                getsym();
+            } else {
+                error(2);
+            }
+        } else {
+            error(3);
+        }
+    } else {
+        error(4);
+    }
+}
+
+void vardeclaration() {
+    if (sym == ident) {
+        enter(variable);
+        getsym();
+    } else {
+        error(4);
+    }
+}
+
+void listcode(long cx0) { // list code generated for this block
+    long i;
+
+    for (i = cx0; i <= cx - 1; i++) {
+        printf("%10d%5s%3d%5d\n", i, mnemonic[code[i].f], code[i].l, code[i].a);
+    }
+
+}
+
+void expression(unsigned long);
+void factor(unsigned long fsys) {
+    long i;
+
+    test(facbegsys, fsys, 24);
+    while (sym & facbegsys) {
+        if (sym == ident) {
+            i = position(id);
+            if (i == 0) {
+                error(11);
+            } else {
+                switch (table[i].kind) {
+                    case constant:
+                        gen(lit, 0, table[i].val);
+                        break;
+                    case variable:
+                        gen(lod, lev - table[i].level, table[i].addr);
+                        break;
+                    case proc:
+                        error(21);
+                        break;
+                }
+            }
+            getsym();
+        } else if (sym == number) {
+            if (num > amax) {
+                error(31);
+                num = 0;
+            }
+            gen(lit, 0, num);
+            getsym();
+        } else if (sym == lparen) {
+            getsym();
+            expression(rparen | fsys);
+            if (sym == rparen) {
+                getsym();
+            } else {
+                error(22);
+            }
+        }
+        test(fsys, lparen, 23);
+    }
+}
+
+void term(unsigned long fsys) {
+    unsigned long mulop;
+
+    factor(fsys | times | slash);
+    while (sym == times || sym == slash) {
+        mulop = sym;
+        getsym();
+        factor(fsys | times | slash);
+        if (mulop == times) {
+            gen(opr, 0, 4);
+        } else {
+            gen(opr, 0, 5);
+        }
+    }
+}
+
+void expression(unsigned long fsys) {
+    unsigned long addop;
+
+    if (sym == plus || sym == minus) {
+        addop = sym;
+        getsym();
+        term(fsys | plus | minus);
+        if (addop == minus) {
+            gen(opr, 0, 1);
+        }
+    } else {
+        term(fsys | plus | minus);
+    }
+    while (sym == plus || sym == minus) {
+        addop = sym;
+        getsym();
+        term(fsys | plus | minus);
+        if (addop == plus) {
+            gen(opr, 0, 2);
+        } else {
+            gen(opr, 0, 3);
+        }
+    }
+}
+
+void condition(unsigned long fsys) {
+    unsigned long relop;
+
+    if (sym == oddsym) {
+        getsym();
+        expression(fsys);
+        gen(opr, 0, 6);
+    } else {
+        expression(fsys | eql | neq | lss | gtr | leq | geq);
+        if (!(sym & (eql | neq | lss | gtr | leq | geq))) {
+            error(20);
+        } else {
+            relop = sym;
+            getsym();
+            expression(fsys);
+            switch (relop) {
+                case eql:
+                    gen(opr, 0, 8);
+                    break;
+                case neq:
+                    gen(opr, 0, 9);
+                    break;
+                case lss:
+                    gen(opr, 0, 10);
+                    break;
+                case geq:
+                    gen(opr, 0, 11);
+                    break;
+                case gtr:
+                    gen(opr, 0, 12);
+                    break;
+                case leq:
+                    gen(opr, 0, 13);
+                    break;
+            }
+        }
+    }
+}
+
+void statement(unsigned long fsys) {
+    long i, cx1, cx2;
+
+    if (sym == ident) {
+        i = position(id);
+        if (i == 0) {
+            error(11);
+        } else if (table[i].kind != variable) { // assignment to non-variable
+            error(12);
+            i = 0;
+        }
+        getsym();
+        if (sym == becomes) {
+            getsym();
+        } else {
+            error(13);
+        }
+        expression(fsys);
+        if (i != 0) {
+            gen(sto, lev - table[i].level, table[i].addr);
+        }
+    } else if (sym == callsym) {
+        getsym();
+        if (sym != ident) {
+            error(14);
+        } else {
+            i = position(id);
+            if (i == 0) {
+                error(11);
+            } else if (table[i].kind == proc) {
+                gen(cal, lev - table[i].level, table[i].addr);
+            } else {
+                error(15);
+            }
+            getsym();
+        }
+    } else if (sym == ifsym) {
+        getsym();
+        condition(fsys | thensym | dosym);
+        if (sym == thensym) {
+            getsym();
+        } else {
+            error(16);
+        }
+        cx1 = cx;
+        gen(jpc, 0, 0);
+        statement(fsys);
+        code[cx1].a = cx;
+    } else if (sym == beginsym) {
+        getsym();
+        statement(fsys | semicolon | endsym);
+        while (sym == semicolon || (sym & statbegsys)) {
+            if (sym == semicolon) {
+                getsym();
+            } else {
+                error(10);
+            }
+            statement(fsys | semicolon | endsym);
+        }
+        if (sym == endsym) {
+            getsym();
+        } else {
+            error(17);
+        }
+    } else if (sym == whilesym) {
+        cx1 = cx;
+        getsym();
+        condition(fsys | dosym);
+        cx2 = cx;
+        gen(jpc, 0, 0);
+        if (sym == dosym) {
+            getsym();
+        } else {
+            error(18);
+        }
+        statement(fsys);
+        gen(jmp, 0, cx1);
+        code[cx2].a = cx;
+    }
+    test(fsys, 0, 19);
+}
+
+void block(unsigned long fsys) {
+    long tx0; // initial table index
+    long cx0; // initial code index
+    long tx1; // save current table index before processing nested procedures
+    long dx1; // save data allocation index
+
+    dx = 3;
+    tx0 = tx;
+    table[tx].addr = cx;
+    gen(jmp, 0, 0);
+    if (lev > levmax) {
+        error(32);
+    }
+    do {
+        if (sym == constsym) {
+            getsym();
+            do {
+                constdeclaration();
+                while (sym == comma) {
+                    getsym();
+                    constdeclaration();
+                }
+                if (sym == semicolon) {
+                    getsym();
+                } else {
+                    error(5);
+                }
+            } while (sym == ident);
+        }
+        if (sym == varsym) {
+            getsym();
+            do {
+                vardeclaration();
+                while (sym == comma) {
+                    getsym();
+                    vardeclaration();
+                }
+                if (sym == semicolon) {
+                    getsym();
+                } else {
+                    error(5);
+                }
+            } while (sym == ident);
+        }
+        while (sym == procsym) {
+            getsym();
+            if (sym == ident) {
+                enter(proc);
+                getsym();
+            } else {
+                error(4);
+            }
+            if (sym == semicolon) {
+                getsym();
+            } else {
+                error(5);
+            }
+            lev = lev + 1;
+            tx1 = tx;
+            dx1 = dx;
+            block(fsys | semicolon);
+            lev = lev - 1;
+            tx = tx1;
+            dx = dx1;
+            if (sym == semicolon) {
+                getsym();
+                test(statbegsys | ident | procsym, fsys, 6);
+            } else {
+                error(5);
+            }
+        }
+        test(statbegsys | ident, declbegsys, 7);
+    } while (sym & declbegsys);
+    code[table[tx0].addr].a = cx;
+    table[tx0].addr = cx; // start addr of code
+    cx0 = cx;
+    gen(Int, 0, dx);
+    statement(fsys | semicolon | endsym);
+    gen(opr, 0, 0); // return
+    test(fsys, 0, 8);
+    listcode(cx0);
+}
+
+long base(long b, long l) {
+    long b1;
+
+    b1 = b;
+    while (l > 0) { // find base l levels down
+        b1 = s[b1];
+        l = l - 1;
+    }
+    return b1;
+}
+
+void interpret() {
+    long p, b, t; // program-, base-, topstack-registers
+    instruction i; // instruction register
+
+    printf("start PL/0\n");
+    t = 0;
+    b = 1;
+    p = 0;
+    s[1] = 0;
+    s[2] = 0;
+    s[3] = 0;
+    do {
+        i = code[p];
+        p = p + 1;
+        switch (i.f) {
+            case lit:
+                t = t + 1;
+                s[t] = i.a;
+                break;
+            case opr:
+                switch (i.a) { // operator
+                    case 0: // return
+                        t = b - 1;
+                        p = s[t + 3];
+                        b = s[t + 2];
+                        break;
+                    case 1:
+                        s[t] = -s[t];
+                        break;
+                    case 2:
+                        t = t - 1;
+                        s[t] = s[t] + s[t + 1];
+                        break;
+                    case 3:
+                        t = t - 1;
+                        s[t] = s[t] - s[t + 1];
+                        break;
+                    case 4:
+                        t = t - 1;
+                        s[t] = s[t] * s[t + 1];
+                        break;
+                    case 5:
+                        t = t - 1;
+                        s[t] = s[t] / s[t + 1];
+                        break;
+                    case 6:
+                        s[t] = s[t] % 2;
+                        break;
+                    case 8:
+                        t = t - 1;
+                        s[t] = (s[t] == s[t + 1]);
+                        break;
+                    case 9:
+                        t = t - 1;
+                        s[t] = (s[t] != s[t + 1]);
+                        break;
+                    case 10:
+                        t = t - 1;
+                        s[t] = (s[t] < s[t + 1]);
+                        break;
+                    case 11:
+                        t = t - 1;
+                        s[t] = (s[t] >= s[t + 1]);
+                        break;
+                    case 12:
+                        t = t - 1;
+                        s[t] = (s[t] > s[t + 1]);
+                        break;
+                    case 13:
+                        t = t - 1;
+                        s[t] = (s[t] <= s[t + 1]);
+                }
+                break;
+            case lod:
+                t = t + 1;
+                s[t] = s[base(b, i.l) + i.a];
+                break;
+            case sto:
+                s[base(b, i.l) + i.a] = s[t];
+                printf("%10d\n", s[t]);
+                t = t - 1;
+                break;
+            case cal: // generate new block mark
+                s[t + 1] = base(b, i.l);
+                s[t + 2] = b;
+                s[t + 3] = p;
+                b = t + 1;
+                p = i.a;
+                break;
+            case Int:
+                t = t + i.a;
+                break;
+            case jmp:
+                p = i.a;
+                break;
+            case jpc:
+                if (s[t] == 0) {
+                    p = i.a;
+                }
+                t = t - 1;
+        }
+    } while (p != 0);
+    printf("end PL/0\n");
+}
+
+void main() {
+    long i;
+    for (i = 0; i < 256; i++) { //Ä³¸öÊý×éµÄ³õÊ¼»¯£¿
         ssym[i] = nul;
     }
-    strcpy(word[0], "begin     "); //word[]å­—ç¬¦ä¸²æ•°ç»„ å­˜æ”¾ä¿ç•™å­—å­—ç¬¦ï¼Ÿ
+    strcpy(word[0], "begin     "); //word[]×Ö·û´®Êý×é ´æ·Å±£Áô×Ö×Ö·û£¿
     strcpy(word[1], "call      ");
     strcpy(word[2], "const     ");
     strcpy(word[3], "do        ");
@@ -27,7 +663,7 @@ int main() {
     strcpy(word[8], "then      ");
     strcpy(word[9], "var       ");
     strcpy(word[10], "while     ");
-    wsym[0] = beginsym; //wsym[]æ— ç¬¦å·é•¿æ•´åž‹æ•°ç»„ å…³é”®å­—å¯¹åº”çš„åå…­è¿›åˆ¶ç ï¼Ÿ
+    wsym[0] = beginsym; //wsym[]ÎÞ·ûºÅ³¤ÕûÐÍÊý×é ¹Ø¼ü×Ö¶ÔÓ¦µÄÊ®Áù½øÖÆÂë£¿
     wsym[1] = callsym;
     wsym[2] = constsym;
     wsym[3] = dosym;
@@ -38,7 +674,7 @@ int main() {
     wsym[8] = thensym;
     wsym[9] = varsym;
     wsym[10] = whilesym;
-    ssym['+'] = plus; //ssym[]ç±»åž‹åŒwsym[] æ“ä½œç¬¦å¯¹åº”çš„åå…­è¿›åˆ¶ç ï¼Ÿ
+    ssym['+'] = plus; //ssym[]ÀàÐÍÍ¬wsym[] ²Ù×÷·û¶ÔÓ¦µÄÊ®Áù½øÖÆÂë£¿
     ssym['-'] = minus;
     ssym['*'] = times;
     ssym['/'] = slash;
@@ -48,7 +684,7 @@ int main() {
     ssym[','] = comma;
     ssym['.'] = period;
     ssym[';'] = semicolon;
-    strcpy(mnemonic[lit], "lit"); //mnemonic[]å­—ç¬¦ä¸²æ•°ç»„ æ”¾ç½®ä¸­é—´ä»£ç æ“ä½œç¬¦çš„å¯¹åº”ç¬¦å·ï¼Ÿ
+    strcpy(mnemonic[lit], "lit"); //mnemonic[]×Ö·û´®Êý×é ·ÅÖÃÖÐ¼ä´úÂë²Ù×÷·ûµÄ¶ÔÓ¦·ûºÅ£¿
     strcpy(mnemonic[opr], "opr");
     strcpy(mnemonic[lod], "lod");
     strcpy(mnemonic[sto], "sto");
@@ -56,7 +692,7 @@ int main() {
     strcpy(mnemonic[Int], "int");
     strcpy(mnemonic[jmp], "jmp");
     strcpy(mnemonic[jpc], "jpc");
-    declbegsys = constsym | varsym | procsym; //æŸç§æ„ä¹‰çš„é•¿æ•´åž‹èµ‹å€¼ï¼Ÿ
+    declbegsys = constsym | varsym | procsym; //Ä³ÖÖÒâÒåµÄ³¤ÕûÐÍ¸³Öµ£¿
     statbegsys = beginsym | callsym | ifsym | whilesym;
     facbegsys = ident | number | lparen;
 
@@ -88,5 +724,4 @@ int main() {
         printf("errors in PL/0 program\n");
     }
     fclose(infile);
-    return 0;
 }
